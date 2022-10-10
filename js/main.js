9aee6332f49bbfd8d369a87136274d3e99925d84
Bryan Hankins <masterpokemo@gmail.com>
2022-10-10 11:06:37 -0500

    Dom

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-function addNewClass(){
-    // Select div element by its id attribute
-    var elem = document.getElementById("hamburger");
-    elem.className += " py-3";
-}
+Highcharts.chart('WORKCHART', {
+    colors: ['#01BAF2', '#71BF45', '#FAA74B', '#B37CD2'],
+    chart: {
+      type: 'pie'
+    },
+    title: {
+     
+    },
+
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '{point.name}: {y} %'
+        },
+        
+      }
+    },
+    series: [{
+      name: 'Percentage',
+      colorByPoint: true,
+      innerSize: '33%',
+      data: [{
+        name: 'Current Deductions',
+        y: 33
+      }, {
+        name: 'Current Taxes',
+        y: 33
+      }, {
+        name: 'Current Earnings',
+        y: 33
+      }]
+    }]
+  });
